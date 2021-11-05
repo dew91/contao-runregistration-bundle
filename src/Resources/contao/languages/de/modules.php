2b49e009c5a4dcfb9f20e7600587a4fa7475cd2f
Dominic Ernst <mail@dominic-ernst.com>
2021-11-05 19:29:13 +0100

    remove obsolete files
    - remove reCaptcha class
    - remove old frontend module language files
    - remove old frontend module template

--- src/Resources/contao/languages/de/modules.php
+++
@@ -1,7 +0,0 @@
-<?php
-	// Deutsche Sprachdatei für Contao-Modul "Laufanmeldung"
-	// Copyright (c) 2016 Dominic Ernst - www.dominic-ernst.com_addref
-	$GLOBALS['TL_LANG']['MOD']['runregistration'] = ['Laufanmeldung', 'Laufanmeldungen verwalten'];
-	
-	//$GLOBALS['TL_LANG']['FMD']['ModuleLaufanmeldungFormular'] = ['Laufanmeldung', 'Zeigt das Anmeldeformular für einen Lauf an'];
-?>
--- src/Resources/contao/languages/de/tl_module.php
+++
@@ -1,3 +0,0 @@
-<?php
-$GLOBALS['TL_LANG']['tl_module']['runregistration'] = ['Lauf', 'Lauf f&uuml;r den das Anmeldeformular g&uuml;ltig ist.'];
-?>
