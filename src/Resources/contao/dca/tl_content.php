0826ae7387d6dd76f55b54042c7559c9ed2c3170
Dominic Ernst <mail@dominic-ernst.com>
2021-11-05 16:02:27 +0100

    configurable content element
    - make runregistration form content element configurable with real 
    values

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -3,9 +3,21 @@
 	$GLOBALS['TL_DCA']['tl_content']['fields']['runregistrationRun'] = array(
 		'exclude'                 => true,
 		'inputType'               => 'select',
-		/*'options_callback'        => array('tl_runregistration_runs', 'getRuns'),*/
-		'options'				  				=> array(1 => 'Run A', 2 => 'Run B'),
+		'options_callback'        => array('tl_content_runregistrationRun', 'getOptions'),
 		'eval'                    => array('mandatory'=>true, 'chosen'=>true, 'submitOnChange'=>true, 'tl_class'=>'w50 wizard'),
 		'sql'                     => "int(10) unsigned NOT NULL default 0"
 	);
+
+	class tl_content_runregistrationRun extends Contao\Backend
+	{
+		public function getOptions()
+		{
+			$runs = array();
+			$objRuns = $this->Database->execute("SELECT id, date, name FROM tl_runregistration_run ORDER BY date ASC");
+			while ($objRuns->next()) {
+				$runs[$objRuns->id] = date('d.m.Y', $objRuns->date) . ': ' . $objRuns->name . ' (ID '.$objRuns->id.')';
+			}
+			return $runs;
+		}
+	}
 ?>
